84f26f0380021f9b0a3daaa61fd6df9261fc801d
IzaiahSun <IzaiahSun@gmail.com>
2024-05-29 02:06:17 +0800

    Update scholar.js

--- static/js/scholar.js
+++ static/js/scholar.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             element.getAttribute('data').split(",").forEach(paperId => {
                 numCitations += data['publications'][paperId]['num_citations']
             })
-            element.innerHTML = numCitations + "cites";
+            element.innerHTML = numCitations + " Cites";
         });
     });
 })
